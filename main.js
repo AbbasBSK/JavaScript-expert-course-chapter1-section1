5976fd506892ede6f2bb16e440c4464154a6df0d
baqaB4731 <baqa4731@gmail.com>
2024-09-21 23:39:02 +0330

    part 15 test

--- main.js
+++ main.js
@@ -1,28 +1,31 @@
-var number1 = prompt("please enter number 1");
-var number2 = prompt("please enter number2");
+// var number1 = prompt("please enter number 1");
+// var number2 = prompt("please enter number2");
 
-var num1 = Number(number1);
-var num2 = Number(number2);
+// var num1 = Number(number1);
+// var num2 = Number(number2);
 
-var sum = num1 + num2;
-var difference = num1 - num2;
-var multiplication  = num1 * num2;
-var division = num1 / num2;
+// var sum = num1 + num2;
+// var difference = num1 - num2;
+// var multiplication  = num1 * num2;
+// var division = num1 / num2;
 
-var equalTo = num1 == num2
-var notEqualTo = num1 != num2
+// var equalTo = num1 == num2
+// var notEqualTo = num1 != num2
 
 
-console.log("جمع دو عدد برابر است با :" + sum);
-console.log("تفریق دو عدد برابر است با :" + difference);
-console.log("ضرب دو عدد برابر است با :" + multiplication);
-console.log("تقسیم دو عدد برابر است با :" + division);
-console.log("آیا دو عدد وارد شده باهم برابرند؟:" + equalTo);
-console.log("آیا دو عدد وارد شده باهم مخالف هستند؟:" + notEqualTo);
+// console.log("جمع دو عدد برابر است با :" + sum);
+// console.log("تفریق دو عدد برابر است با :" + difference);
+// console.log("ضرب دو عدد برابر است با :" + multiplication);
+// console.log("تقسیم دو عدد برابر است با :" + division);
+// console.log("آیا دو عدد وارد شده باهم برابرند؟:" + equalTo);
+// console.log("آیا دو عدد وارد شده باهم مخالف هستند؟:" + notEqualTo);
 
-function test() {
-    console.log(5555);
-    
-}
 
-test();
+console.log(Boolean(10 > 5));
+
+console.log ( Boolean(1) ); // true
+console.log ( Boolean(0) ); // false
+
+console.log ( Boolean("hello") ); // true
+console.log ( Boolean("") ); // false
+
