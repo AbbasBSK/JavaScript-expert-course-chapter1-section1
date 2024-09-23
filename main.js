24539ba581185ae04c01dc1612a6fdadad6603df
baqaB4731 <baqa4731@gmail.com>
2024-09-23 22:52:18 +0330

    difference of var++ with ++var

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
-console.log(Boolean(0));//true
-var change = false;
+// console.log(Boolean(0));//true
+// var change = false;
 
-var numChange = Number(change);
+// var numChange = Number(change);
 
-console.log(numChange);
+// console.log(numChange);
 
-console.log(Boolean("0"));
+// console.log(Boolean("0"));
+
+
+var a = 1
+
+var b = ++a;
+console.log(b);
+b = ++a
+console.log(b);
+
+
 
 
 
