db86936c572bde84d18c705c988f8426b21761db
baqaB4731 <baqa4731@gmail.com>
2024-09-27 23:55:13 +0330

    test comparison of string

--- main.js
+++ main.js
@@ -1,40 +1,43 @@
-var weight = prompt("لطفا وزن خود را به کیلوگرم وارد کنید");
-var height = prompt("لطفا قد خود را به متر وارد کنید");
-var gender = prompt("لطفا جنسیت خود را با یکی از واژه های رو برو وارد کنید:" +  "آقا" + " - " + "خانم" );
+// var weight = prompt("لطفا وزن خود را به کیلوگرم وارد کنید :");
+// var height = prompt("لطفا قد خود را به سانتیمتر وارد کنید :");
+// var gender = prompt('لطفا جنسیت خود را با نوشتن یکی از موارد روبرو وارد کنید' + " خانم " + " - " +" آقا ");
 
+// var uweight = Number(weight);
+// var uheight = Number(height)/100;
 
-var uweight = Number(weight);
-var uheight = Number(height)/100;
+// var BMI = uweight/uheight**2
 
-var BMI = uweight/(uheight**2);
+// alert(BMI);
 
+// if (gender == "آقا") {
 
-
-if (gender == "آقا") {
-
-    if (BMI >= 18 && BMI <25) {
-        alert("وزن شما متناسب می باشد" + " " + BMI);
-    }else if(BMI < 18 ){
-        alert("شما لاغر هستید" + " " + BMI);
-    }else{
-        alert("شما اضافه وزن دارید" + " " + BMI);
-    }
-
-} else if (gender == "خانم"){
-    if (BMI >= 20 && BMI <28) {
-        alert("وزن شما متناسب می باشد" + " " + BMI);
-    }else if(BMI < 20 ){
-        alert("شما لاغر هستید" + " " + BMI);
-    }else{
-        alert("شما اضافه وزن دارید" + " " + BMI);
-    }
-
-}else{
-    alert("!!اطلاعات وارد شده صحیح نمی باشد");
-}
+//     if (BMI >= 18 && BMI < 25 ) {
+//         alert("شما دارای وزن متناسب هستید");
+//     } else if(BMI < 18){
+//         alert("شما دارای کمبود وزن هستید");
+//     }else{
+//         alert("شما دارای اضافه وزن هستید");
+//     }
+// }else if (gender == "خانم") {
+//     if (BMI >= 20 && BMI < 28 ) {
+//         alert("شما دارای وزن متناسب هستید");
+//     } else if(BMI < 20){
+//         alert("شما دارای کمبود وزن هستید");
+//     }else{
+//         alert("شما دارای اضافه وزن هستید");
+//     }
+// }
 
 
 
+var num = 5;
+
+++num;
+
+alert(num)
+
+alert("a" > "b")
+alert("a" < "b")
 
 
 
