754a20af315d2fdc9389ef2e0afeabbc5fe792b0
baqaB4731 <baqa4731@gmail.com>
2024-09-28 23:49:42 +0330

    part 21 , 22 - 21-22 comparison string - ++var - var++

--- main.js
+++ main.js
@@ -1,43 +1,42 @@
-// var weight = prompt("لطفا وزن خود را به کیلوگرم وارد کنید :");
-// var height = prompt("لطفا قد خود را به سانتیمتر وارد کنید :");
-// var gender = prompt('لطفا جنسیت خود را با نوشتن یکی از موارد روبرو وارد کنید' + " خانم " + " - " +" آقا ");
-
-// var uweight = Number(weight);
-// var uheight = Number(height)/100;
-
-// var BMI = uweight/uheight**2
-
-// alert(BMI);
-
-// if (gender == "آقا") {
-
-//     if (BMI >= 18 && BMI < 25 ) {
-//         alert("شما دارای وزن متناسب هستید");
-//     } else if(BMI < 18){
-//         alert("شما دارای کمبود وزن هستید");
-//     }else{
-//         alert("شما دارای اضافه وزن هستید");
-//     }
-// }else if (gender == "خانم") {
-//     if (BMI >= 20 && BMI < 28 ) {
-//         alert("شما دارای وزن متناسب هستید");
-//     } else if(BMI < 20){
-//         alert("شما دارای کمبود وزن هستید");
-//     }else{
-//         alert("شما دارای اضافه وزن هستید");
-//     }
-// }
-
-
-
 var num = 5;
 
-++num;
+num = num + 1; // 6
 
-alert(num)
+num++; //7
+num++; //8
 
-alert("a" > "b")
-alert("a" < "b")
+++num ; //9
+
+console.log(num++);// num = num + 1
+console.log(num++); // 6
+console.log(num); //7
+
+console.log(++num);// 1+num = num
+console.log(num); //6
+
+console.log(num--);
+console.log(num);
+
+
+var num2 = num-- // num2 = num = num - 1
+
+console.log(num2);// 5
+console.log(num);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
