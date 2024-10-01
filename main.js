1af54cb54fba6826be83f7700f9a20faca6ee7ca
baqaB4731 <baqa4731@gmail.com>
2024-10-02 00:28:41 +0330

    function test2

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
-var num = 5;
+// var num = 5;
 
-num = num + 1; // 6
+// num = num + 1; // 6
 
-num++; //7
-num++; //8
+// num++; //7
+// num++; //8
 
-++num ; //9
+// ++num ; //9
 
-console.log(num++);// num = num + 1
-console.log(num++); // 6
-console.log(num); //7
+// console.log(num++);// num = num + 1
+// console.log(num++); // 6
+// console.log(num); //7
 
-console.log(++num);// 1+num = num
-console.log(num); //6
+// console.log(++num);// 1+num = num
+// console.log(num); //6
 
-console.log(num--);
-console.log(num);
+// console.log(num--);
+// console.log(num);
 
 
-var num2 = num-- // num2 = num = num - 1
+// var num2 = num-- // num2 = num = num - 1
 
-console.log(num2);// 5
-console.log(num);
+// console.log(num2);// 5
+// console.log(num);
 function start () {
     return 2+3;
 }
