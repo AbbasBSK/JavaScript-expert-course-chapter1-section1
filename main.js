d2f4033e38b44bae3444e2449b164f6711a08db2
baqaB4731 <baqa4731@gmail.com>
2024-10-03 23:27:21 +0330

    function without return- test

--- main.js
+++ main.js
@@ -1,29 +1,5 @@
-// var num = 5;
-
-// num = num + 1; // 6
-
-// num++; //7
-// num++; //8
-
-// ++num ; //9
-
-// console.log(num++);// num = num + 1
-// console.log(num++); // 6
-// console.log(num); //7
-
-// console.log(++num);// 1+num = num
-// console.log(num); //6
-
-// console.log(num--);
-// console.log(num);
-
-
-// var num2 = num-- // num2 = num = num - 1
-
-// console.log(num2);// 5
-// console.log(num);
 function start () {
-    return 2+3;
+    2+3;
 }
 
 
