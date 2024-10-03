a5d764c31f916adc265c2b07748990f6cdd66b95
baqaB4731 <baqa4731@gmail.com>
2024-10-03 23:46:27 +0330

    part25 - function

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-function start () {
-    2+3;
+
+function sayHello() {
+    var num1 = 2;
+    var num2 = 5;
+   alert(num1 * num2);
 }
 
+// var num1 = prompt("لطفا یک عدد وارد کنید:");
 
-alert(start());
-
-
+sayHello();
 
 
 
