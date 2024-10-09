d7352feb7a7ffaa3f365d8cc34812fa1bd3dfcac
baqaB4731 <baqa4731@gmail.com>
2024-10-09 22:43:18 +0330

    part 26 - function by params

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-function test(a , b , c) {
-  var name = a;
-  var age = b;
-
-  alert(`my nam is ${name} and my age is ${age} years old`);
-  
-}
 
 var yourName = prompt(`please insert your name :`);
 var yourAge = prompt(`please insert your age ;`);
 var bool = confirm("enter ok or cancel :")
+
+function test(a , b , c) {
+  var name = a;
+  var age = b;
+  
+  if (c) {
+    alert(`my nam is ${name} and my age is ${age} years old`);
+  }
+}
+
 
 test(yourName , yourAge , c);
 
