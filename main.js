d167606b7e7028c89daca26d5b6222f8a35e21c4
baqaB4731 <baqa4731@gmail.com>
2024-10-13 23:40:12 +0330

    part 30 - practice of calculator

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
-function test1() {
-  var num1 = 5;
-  var num2 = 8;
+var num1 = Number(prompt("enter number1 :"));
+var num2 = Number(prompt("enter number2 :"));
 
-   
-  return(10 > 9);
-}
+var sumString = "sum";
+var minString = "min";
+var mulString = "mul";
+var divString = "div";
 
-alert(test1());
+
+var operator = prompt(`please type operator:
+  ${sumString}
+  ${minString}
+  ${mulString}
+  ${divString}`);
+
+
+  function result(a) {
+    return(`yor answer is ${a}`) ;
+  }
+
+
+  if (operator == sumString) {
+   alert(result(num1 + num2)); 
+  }else if (operator == minString) {
+    alert(result(num1 - num2)); 
+  }else if (operator == mulString) {
+    alert(result(num1 * num2)); 
+  }else if (operator == divString) {
+    alert(result(num1 / num2)); 
+  }else{
+    alert("yor type is wrong...!")
+  }
 
 
 
