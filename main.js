aab203274a5d619510e6284d2179388f159d7301
baqaB4731 <baqa4731@gmail.com>
2024-10-14 00:13:54 +0330

    test calculator

--- main.js
+++ main.js
@@ -1,66 +1,35 @@
 var num1 = Number(prompt("enter number1 :"));
 
+var sumstring = "+";
+var minString  = "-";
+var mulString  = "*";
+var divString  = "/";
 
-var sumString = "+";
-var minString = "-";
-var mulString = "*";
-var divString = "/";
-
-var operator = prompt(`please type operator:
-  ${sumString}
+var operator = prompt(`please type operator :
+  ${sumstring}
   ${minString}
   ${mulString}
-  ${divString}`);
+  ${divString}
+  `)
+
+  var num2 = Number(prompt("enter number2 :"));
 
 
-var num2 = Number(prompt("enter number2 :"));
-
-
-  function result(a) {
-    return(`yor answer is ${a}`) ;
+  function result (a) {
+    return(`your answer is ${a} `)
   }
 
-
-  if (operator == sumString) {
-   alert(result(num1 + num2)); 
+  if (operator == sumstring) {
+    alert(result(num1 + num2));
   }else if (operator == minString) {
-    alert(result(num1 - num2)); 
+    alert(result(num1 - num2));
   }else if (operator == mulString) {
-    alert(result(num1 * num2)); 
+    alert(result(num1 * num2));
   }else if (operator == divString) {
-    alert(result(num1 / num2)); 
+    alert(result(num1 / num2));
   }else{
-    alert("yor type is wrong...!")
+    alert("yor type is wrong...!");
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
