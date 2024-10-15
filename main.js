707d0497e8d9e98356e6e1f811fa5b0c46268ab0
baqaB4731 <baqa4731@gmail.com>
2024-10-15 23:45:49 +0330

    example in javascript.info

--- main.js
+++ main.js
@@ -1,35 +1,18 @@
-var num1 = Number(prompt("enter number1 :"));
+var a = +prompt('a?', '');
 
-var sumstring = "+";
-var minString  = "-";
-var mulString  = "*";
-var divString  = "/";
+switch (a) {
+  case 0:
+    alert( 0 );
+    break;
 
-var operator = prompt(`please type operator :
-  ${sumstring}
-  ${minString}
-  ${mulString}
-  ${divString}
-  `)
+  case 1:
+    alert( 1 );
+    break;
 
-  var num2 = Number(prompt("enter number2 :"));
-
-
-  function result (a) {
-    return(`your answer is ${a} `)
-  }
-
-  if (operator == sumstring) {
-    alert(result(num1 + num2));
-  }else if (operator == minString) {
-    alert(result(num1 - num2));
-  }else if (operator == mulString) {
-    alert(result(num1 * num2));
-  }else if (operator == divString) {
-    alert(result(num1 / num2));
-  }else{
-    alert("yor type is wrong...!");
-  }
-
-
-
+  case 2:
+  case 3:
+    alert( '2,3' );
+    break;
+    default:
+      alert( 'wrong..........!' );
+}
