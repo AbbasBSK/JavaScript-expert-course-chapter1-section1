773aec8be02fbe2369be4b2cde2523b99abae2e0
baqaB4731 <baqa4731@gmail.com>
2024-10-16 00:20:33 +0330

    part 31 - switch

--- main.js
+++ main.js
@@ -1,18 +1,48 @@
-var a = +prompt('a?', '');
+var day = prompt("لطفا عدد یکی از روزهای هفته را وارد کنید:");
 
-switch (a) {
-  case 0:
-    alert( 0 );
+switch (day) {
+  case "0":
+     alert("شنبه");
+    break;
+  case "1":
+     alert("یکشنبه");
+    break;
+  case "2":
+     alert("دوشنبه");
+    break;
+  case "3":
+    alert("سه شنبه");
+    break;
+   case "4":
+     alert("چهارشنبه");
+    break;
+  case "5":
+     alert("پنج شنبه");
+    break;
+  case "6":
+     alert("جمعه");
     break;
 
-  case 1:
-    alert( 1 );
+  default:
+    alert("عدد وارد شده صحیح نیست");
     break;
-
-  case 2:
-  case 3:
-    alert( '2,3' );
-    break;
-    default:
-      alert( 'wrong..........!' );
 }
+
+
+// if (day === 0) {
+//   alert("شنبه");
+// }else if (day == 1) {
+//   alert("یکشنبه");
+// }else if (day == 2) {
+//   alert("دوشنبه");
+// }else if (day == 3) {
+//   alert("سه شنبه");
+// }else if (day == 4) {
+//   alert("چهارشنبه");
+// }else if (day == 5) {
+//   alert("پنج شنبه");
+// }else if (day == 6) {
+//   alert("جمعه");
+// }else {
+//   alert("عدد وارد شده صحیح نیست");
+// }
